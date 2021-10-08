3869b25611aabc95488589b6efdbddb26e8cac07
rus <rus06bik@gmail.com>
2021-10-08 10:51:47 +0300

    bug fixes

--- Диплом/diplom.py
+++ Диплом/diplom.py
@@ -4,29 +4,65 @@ from time import sleep
 from tqdm import tqdm
 import json
 
+class Vkphotos():
 
-def find_photos(vk_id):
-    photos_dict = {}
-    token_vk = "958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008"
-    URL = "https://api.vk.com/method/photos.get"
-    params = {
-        'album_id': 'profile',
-        'photo_sizes': 1,
-        "access_token": token_vk,
-        "user_id": vk_id,
-        'v': '5.131',
-        'extended': 1
-    }
-    res = requests.get(URL, params=params).json()
-    for photo in res['response']['items']:
+    def find_photos(self, vk_id, count,  token_vk="958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008" ):
+        photos_dict = {}
 
-        sizes_list = photo['sizes']
-        photos_dict[f"{photo['likes']['count']}.jpg"] = [sizes_list[-1]['url'], sizes_list[-1]['type']]
-    return photos_dict
+        if type(vk_id) is int:
+            URL = "https://api.vk.com/method/photos.get"
+            params = {
+                'album_id': 'profile',
+                'photo_sizes': 1,
+                "access_token": token_vk,
+                "user_id": vk_id,
+                'v': '5.131',
+                'extended': 1,
+                'count': count
+            }
+            res = requests.get(URL, params=params).json()
+            for photo in res['response']['items']:
+                sizes_list = photo['sizes']
+                photos_dict[f"{photo['likes']['count']}_{photo['date']}.jpg"] = [sizes_list[-1]['url'], sizes_list[-1]['type']]
+            print("search has ended")
+        else:
+            id_from_numbers = None
+            URL = "https://api.vk.com/method/users.get"
+            params = {
+                'user_ids': vk_id,
+                'v':'5.131',
+                "access_token": token_vk
+            }
+            response = requests.get(URL, params=params).json()
+            # print(response)
+            try:
+                if response['response']:
+                    for vk_id_for_photo in response['response']:
+                        id_from_numbers = vk_id_for_photo['id']
+                        print("id has received")
+            except KeyError:
+                print("Проверьте vk id")
+            URL = "https://api.vk.com/method/photos.get"
+            params = {
+                'album_id': 'profile',
+                'photo_sizes': 1,
+                "access_token": token_vk,
+                "user_id": id_from_numbers,
+                'v': '5.131',
+                'extended': 1,
+                'count': count
+            }
+            res = requests.get(URL, params=params).json()
+            for photo in res['response']['items']:
+                sizes_list = photo['sizes']
+                photos_dict[f"{photo['likes']['count']}_{photo['date']}.jpg"] = [sizes_list[-1]['url'], sizes_list[-1]['type']]
+            print("search has ended")
+
+        return photos_dict
 
 
 class YaUploader():
-    def __init__(self, token: str):
+    def __init__(self,token: str):
         self.token = token
 
     def add_folder(self, vk_id):
@@ -43,7 +79,7 @@ class YaUploader():
 
         return vk_id
 
-    def _get_upload_link_and_upload(self, name, link,folder):
+    def _upload(self, name, link,folder):
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
 
         headers = {
@@ -81,27 +117,28 @@ def transform_to_json(photos_dict):
     with open("output.json", 'w') as file:
         json.dump(d, file)
 
+# я хотел вызвать фунцкию и передать ей "info", т.к "info" содержит как token_vk так token_Ya
+# def function_call(token_vk, token_Ya):
+#     pass
 
 if __name__ == "__main__":
+    # begemot_korovin
+    # по этому фрагменту хотел достать содержимое файла и его использовать
+    # with open('tokens.txt', "r") as file:
+    #     info = file.read()
+    # print(S)
+    # function_call(S)
+
+    token_Ya = "AQAAAAAm01XcAADLW6GikyrnJkfiqQqS_F5dSgw"
+
     if True:
-        vk_id = input("Введите id Вконтакте: ")
-        token_vk = "958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008"
-        URL = "https://api.vk.com/method/users.get"
-        params = {
-            "access_token": token_vk,
-            "user_id": vk_id
-        }
-        response = requests.get(URL, params=params)
-        if response.status_code != 200:
-            print('Проверьте Vk id: ')
-    # 552934290
-    # token = input("Введите токен для Я.Диска: ")
-        else:
-            photos = find_photos(vk_id)
-            token = ""
-            uploader = YaUploader(token)
-            transform_to_json(photos)
-            for key, value in tqdm(photos.items()):
-                uploader.add_folder(vk_id)
-                uploader._get_upload_link_and_upload(key, value[0],vk_id)
-                sleep(1)
+        vk_id = input('Введите vk id: ')
+        count = int(input("Введите кол-во записей для загрузки: "))
+        photos = Vkphotos().find_photos(vk_id, count)
+        Vkphotos().find_photos(vk_id, count)
+        uploader = YaUploader(token_Ya)
+        transform_to_json(photos)
+        for key, value in tqdm(photos.items()):
+            uploader.add_folder(vk_id)
+            uploader._upload(key, value[0],vk_id)
+            sleep(1)
